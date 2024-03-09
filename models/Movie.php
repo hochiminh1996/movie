6d38874bebf02ea4373e7cc84a551e3a2ea5bf11
hochiminh1996 <ambrsp@hotmail.com>
2024-03-09 17:33:57 -0300

    Fazendo o model de filme

---
+++ models/Movie.php
@@ -0,0 +1,53 @@
+<?php 
+
+class Movie{
+    public $id;
+    public $title;
+    public $description;
+    public $image;
+    public $trailer;
+    public $category;
+    public $length;
+    public $users_id;
+
+    public function imageGenerateName(){
+        return bin2hex(random_bytes(60)). "jpg";
+    }
+}
+
+// interface que manda no DAO
+interface MovieDaoInterface{
+    // construindo um objeto de movie com base nos dados recebidos
+    public function buildMovie($data);
+
+    // pega todos os filmes do banco
+    public function findAll();
+
+    // pegar os filmes inseridos mais recentementes : ou seja, decrescente
+    public function getLatestMovie();
+
+    // pegar os filmes por categoria
+    public function getMoviesByCategory($category);
+
+    // pegar os fillmes de um usuário específico
+    public function getMoviesByUserId($id);
+
+    // pegando um filme por id
+    public function findById($id);
+
+    // pegando um filme por title
+    public function findByTitle($title);
+
+    // registrando um filme
+    public function create(Movie $movie);
+    
+    // atualizando um filem
+    public function update(Movie $movie);
+
+    // deletar um filme
+    public function destroy(Movie $movie);
+
+}
+
+
+?>
